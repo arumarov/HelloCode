4342e84f592751a40054f1b6f895236db731652c
Arumarov <arumarov@yandex.ru>
2021-10-30 18:12:09 +0600

    add Example 010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 12, 31, 4, 18, 16, 17, 18}; // чисел может быть сколько угодно
+
+int n = array.Length; // длина (количество элементов) массива
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
